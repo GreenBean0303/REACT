1a309663db1431f571acf2eaa6c46f61198c1f26
GreenBean0303 <lootsmannagnes@gmail.com>
2025-01-23 14:25:14 +0200

    Punkt 2ni tehtud

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ExpenseItem from './components/ExpenseItem.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div className = "App"><ExpenseItem />
+    </div>
+  );
 }
 
 export default App
---
+++ React/src/components/ExpenseItem.jsx
@@ -0,0 +1,13 @@
+import './ExpenseItem'
+const ExpenseItem = () => {
+    
+    return (
+        <div className = "expense-item">
+            <div className = "expense-item_description">
+                <h2>Item</h2>
+                <div className = "expense-item_price">Price</div>
+            </div>
+         </div>
+    )
+}
+export default ExpenseItem
